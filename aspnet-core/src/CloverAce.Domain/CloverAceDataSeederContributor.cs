738c3b97fa083e21a451fe50c28ae4a917328600
evaristocuesta <evaristocg@gmail.com>
2023-04-21 20:18:49 +0200

    Add account repository tests

---
+++ aspnet-core/src/CloverAce.Domain/CloverAceDataSeederContributor.cs
@@ -0,0 +1,34 @@
+﻿using CloverAce.Accounts;
+using System.Threading.Tasks;
+using Volo.Abp.Data;
+using Volo.Abp.DependencyInjection;
+
+namespace CloverAce;
+public class CloverAceDataSeederContributor
+    : IDataSeedContributor, ITransientDependency
+{
+    private readonly IAccountRepository _accountRepository;
+    private readonly AccountManager _accountManager;
+
+    public CloverAceDataSeederContributor(
+        IAccountRepository accountRepository, 
+        AccountManager accountManager)
+    {
+        _accountRepository = accountRepository;
+        _accountManager = accountManager;
+    }
+
+    public async Task SeedAsync(DataSeedContext context)
+    {
+        if (await _accountRepository.GetCountAsync() > 0) 
+        {
+            return;
+        }
+
+        var ingDirect = await _accountRepository.InsertAsync(
+            await _accountManager.CreateAsync("ING Direct"));
+
+        var evo = await _accountRepository.InsertAsync(
+            await _accountManager.CreateAsync("Evo Bank"));
+    }
+}
---
+++ aspnet-core/test/CloverAce.EntityFrameworkCore.Tests/EntityFrameworkCore/Accounts/AccountRepositoryTests.cs
@@ -0,0 +1,59 @@
+﻿using CloverAce.Accounts;
+using Shouldly;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CloverAce.EntityFrameworkCore.Accounts;
+
+public class AccountRepositoryTests : CloverAceEntityFrameworkCoreTestBase
+{
+    private readonly IAccountRepository _accountRepository;
+    private readonly AccountManager _accountManager;
+
+    public AccountRepositoryTests()
+    {
+        _accountRepository = GetRequiredService<IAccountRepository>();
+        _accountManager = GetRequiredService<AccountManager>();
+    }
+
+    [Fact]
+    public async Task Should_Exist()
+    {
+        await WithUnitOfWorkAsync(async () =>
+        {
+            // Act
+            var exists = await _accountRepository.ExistsAsync("ING Direct");
+
+            // Assert
+            exists.ShouldBeTrue();
+        });
+    }
+
+    [Fact]
+    public async Task Should_Get_All_Accounts()
+    {
+        // Act
+        var accounts = await _accountRepository.GetListAsync();
+
+        // Assert
+        accounts.Count.ShouldBe(2);
+        accounts[0].Name.ShouldBe("ING Direct");
+        accounts[1].Name.ShouldBe("Evo Bank");
+    }
+
+    [Fact]
+    public async Task Should_Create_A_New_Accout()
+    {
+        // Act
+        var account = await _accountRepository.InsertAsync(
+            await _accountManager.CreateAsync("BBVA"), 
+            true);
+
+        // Assert
+        var checkAccount = await _accountRepository.GetAsync(account.Id);
+        checkAccount.ShouldNotBeNull();
+        checkAccount.Id.ShouldBe(account.Id);
+        checkAccount.Name.ShouldBe(account.Name);
+    }
+}
