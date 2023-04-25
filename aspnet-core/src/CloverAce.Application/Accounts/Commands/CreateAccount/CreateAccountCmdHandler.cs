943832b4de02b689eb84ca9f1b8d3b39b70a1481
evaristocuesta <evaristocg@gmail.com>
2023-04-25 21:25:22 +0200

    Add MediatR

---
+++ aspnet-core/src/CloverAce.Application/Accounts/Commands/CreateAccount/CreateAccountCmdHandler.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CloverAce.Accounts.Commands.CreateAccount
+{
+    public class CreateAccountCmdHandler : IRequestHandler<CreateAccountCmd, CreateAccountCmdResponse>
+    {
+        private readonly IAccountRepository _accountRepository;
+        private readonly AccountManager _accountManager;
+        private readonly IMapper _mapper;
+
+        public CreateAccountCmdHandler(
+            IAccountRepository accountRepository, 
+            AccountManager accountManager, 
+            IMapper mapper)
+        {
+            _accountRepository = accountRepository;
+            _accountManager = accountManager;
+            _mapper = mapper;
+        }
+
+        public async Task<CreateAccountCmdResponse> Handle(CreateAccountCmd request, CancellationToken cancellationToken)
+        {
+            var account = await _accountRepository.InsertAsync(
+                await _accountManager.CreateAsync(request.Name), 
+                autoSave: true, 
+                cancellationToken);
+
+            return new CreateAccountCmdResponse { Account = _mapper.Map<AccountDto>(account) };
+        }
+    }
+}
