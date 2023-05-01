673159e453b22d4a0d5569c6656b87f8dbd19adf
evaristocuesta <evaristocg@gmail.com>
2023-05-01 13:29:06 +0200

    Refactor code

--- aspnet-core/src/CloverAce.Application/Accounts/Commands/CreateAccount/CreateAccountCmdHandler.cs
+++ aspnet-core/src/CloverAce.Application/Accounts/Commands/CreateAccount/CreateAccountCmdHandler.cs
@@ -4,34 +4,33 @@ using System.Threading;
 using System.Threading.Tasks;
 using Volo.Abp.AutoMapper;
 
-namespace CloverAce.Accounts.Commands.CreateAccount
+namespace CloverAce.Accounts.Commands.CreateAccount;
+
+public class CreateAccountCmdHandler : IRequestHandler<CreateAccountCmd, AccountDto>
 {
-    public class CreateAccountCmdHandler : IRequestHandler<CreateAccountCmd, AccountDto>
+    private readonly IAccountRepository _accountRepository;
+    private readonly AccountManager _accountManager;
+    private readonly IMapper _mapper;
+
+    public CreateAccountCmdHandler(
+        IAccountRepository accountRepository, 
+        AccountManager accountManager, 
+        IMapperAccessor mapper)
     {
-        private readonly IAccountRepository _accountRepository;
-        private readonly AccountManager _accountManager;
-        private readonly IMapper _mapper;
+        _accountRepository = accountRepository;
+        _accountManager = accountManager;
+        _mapper = mapper.Mapper;
+    }
 
-        public CreateAccountCmdHandler(
-            IAccountRepository accountRepository, 
-            AccountManager accountManager, 
-            IMapperAccessor mapper)
-        {
-            _accountRepository = accountRepository;
-            _accountManager = accountManager;
-            _mapper = mapper.Mapper;
-        }
+    public async Task<AccountDto> Handle(CreateAccountCmd request, CancellationToken cancellationToken)
+    {
+        var account = await _accountManager.CreateAsync(request.Name, cancellationToken);
 
-        public async Task<AccountDto> Handle(CreateAccountCmd request, CancellationToken cancellationToken)
-        {
-            var account = await _accountManager.CreateAsync(request.Name, cancellationToken);
+        await _accountRepository.InsertAsync(
+            account, 
+            autoSave: true, 
+            cancellationToken);
 
-            await _accountRepository.InsertAsync(
-                account, 
-                autoSave: true, 
-                cancellationToken);
-
-            return _mapper.Map<AccountDto>(account);
-        }
+        return _mapper.Map<AccountDto>(account);
     }
 }
