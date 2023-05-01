2cb94283760a615b1fae56671b43de10753746ce
evaristocuesta <evaristocg@gmail.com>
2023-05-01 14:46:29 +0200

    Convert to file scoped namespace

--- aspnet-core/src/CloverAce.HttpApi/Controllers/Accounts/AccountsController.cs
+++ aspnet-core/src/CloverAce.HttpApi/Controllers/Accounts/AccountsController.cs
@@ -1,5 +1,4 @@
 ﻿using CloverAce.Accounts;
-using CloverAce.Accounts.Commands;
 using CloverAce.Accounts.Commands.CreateAccount;
 using CloverAce.Accounts.Commands.UpdateAccount;
 using Microsoft.AspNetCore.Http;
@@ -8,40 +7,39 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace CloverAce.Controllers.Accounts
+namespace CloverAce.Controllers.Accounts;
+
+[ApiController]
+[Route("api/accounts")]
+public class AccountsController : CloverAceController
 {
-    [ApiController]
-    [Route("api/accounts")]
-    public class AccountsController : CloverAceController
+    public AccountsController(IServiceProvider serviceProvider) : base(serviceProvider)
     {
-        public AccountsController(IServiceProvider serviceProvider) : base(serviceProvider)
-        {
-        }
+    }
 
-        [HttpPost]
-        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(AccountDto))]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        [ProducesResponseType(StatusCodes.Status504GatewayTimeout)]
-        public async Task<ActionResult<AccountDto>> Post(
-            [FromBody] CreateAccountCmd cmd, 
-            CancellationToken cancellationToken)
-        {
-            var response = await Mediator.Send(cmd, cancellationToken);
-            return Ok(response);
-        }
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(AccountDto))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    [ProducesResponseType(StatusCodes.Status504GatewayTimeout)]
+    public async Task<ActionResult<AccountDto>> Post(
+        [FromBody] CreateAccountCmd cmd,
+        CancellationToken cancellationToken)
+    {
+        var response = await Mediator.Send(cmd, cancellationToken);
+        return Ok(response);
+    }
 
-        [HttpPut]
-        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(AccountDto))]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        [ProducesResponseType(StatusCodes.Status504GatewayTimeout)]
-        public async Task<ActionResult<AccountDto>> Post(
-            [FromBody] UpdateAccountCmd cmd, 
-            CancellationToken cancellationToken)
-        {
-            var response = await Mediator.Send(cmd, cancellationToken);
-            return Ok(response);
-        }
+    [HttpPut]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(AccountDto))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    [ProducesResponseType(StatusCodes.Status504GatewayTimeout)]
+    public async Task<ActionResult<AccountDto>> Post(
+        [FromBody] UpdateAccountCmd cmd,
+        CancellationToken cancellationToken)
+    {
+        var response = await Mediator.Send(cmd, cancellationToken);
+        return Ok(response);
     }
 }
